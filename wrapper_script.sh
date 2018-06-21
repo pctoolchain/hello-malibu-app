0dea11859bb962b761fa865ba60df0034c09cda7
Public CTN <public.cloud.toolchain@gmail.com>
2018-06-21 15:16:03 +0100

    Introduce Memory Leak

--- wrapper_script.sh
+++ wrapper_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 java -jar ./target/spinnaker-demo-1.0.jar &
-#./alertmanager-0.15.0-rc.3.linux-amd64/alertmanager &
+./alertmanager-0.15.0-rc.3.linux-amd64/alertmanager &
 ./node_exporter-0.16.0.linux-amd64/node_exporter
